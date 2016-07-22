cadc936cfd18db0516228b2d3518305712685a23
Michaël Thieulin <m.thieulin@monsieurbiz.com>
2016-07-22 02:42:12 +0200

    Update Facebook PHP SDK to v5

--- src/Erivello/Silex/Provider/FacebookServiceProvider.php
+++ src/Erivello/Silex/Provider/FacebookServiceProvider.php
@@ -13,38 +13,37 @@ namespace Erivello\Silex\Provider;
 
 use Silex\Application;
 use Silex\ServiceProviderInterface;
+use Facebook;
 
 /**
- * Facebook Provider
+ * Facebook Provider.
  *
  * @author Edoardo Rivello <edoardo.rivello@gmail.com>
  */
 class FacebookServiceProvider implements ServiceProviderInterface
 {
     /**
-     * @{inheritDoc}
+     * {@inheritdoc}
      */
     public function register(Application $app)
     {
-        $app->before(function() use ($app) {
+        $app->before(function () use ($app) {
             foreach ($app['facebook.apps'] as $label => $facebookApp) {
-
-                $app['facebook_' . $label] = $app->share(function() use ($facebookApp) {
-                    return new \Facebook(array(
-                        'appId' => $facebookApp['facebook.app_id'],
-                        'secret' => $facebookApp['facebook.secret'],
+                $app['facebook_'.$label] = $app->share(function () use ($facebookApp) {
+                    return new Facebook\Facebook(array(
+                        'app_id' => $facebookApp['facebook.app_id'],
+                        'app_secret' => $facebookApp['facebook.secret'],
+                        'default_graph_version' => 'v2.6',
                     ));
                 });
-
             }
         });
     }
-    
+
     /**
-     * @{inheritDoc}
+     * {@inheritdoc}
      */
     public function boot(Application $app)
     {
-        
-    }    
+    }
 }
--- tests/Erivello/Silex/Provider/Tests/FacebookServiceProviderTest.php
+++ tests/Erivello/Silex/Provider/Tests/FacebookServiceProviderTest.php
@@ -5,6 +5,7 @@ namespace Erivello\Silex\Tests;
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Erivello\Silex\Provider\FacebookServiceProvider;
+use Facebook;
 
 class FacebookServiceProviderTest extends \PHPUnit_Framework_TestCase
 {
@@ -15,31 +16,31 @@ class FacebookServiceProviderTest extends \PHPUnit_Framework_TestCase
         $app->register(new FacebookServiceProvider(), array(
             'facebook.apps' => array(
                 'test' => array(
-                    'facebook.app_id'     => '12345',
-                    'facebook.secret'     => '67890'
+                    'facebook.app_id' => '12345',
+                    'facebook.secret' => '67890',
                 ),
                 'foo' => array(
-                    'facebook.app_id'     => '54321',
-                    'facebook.secret'     => '09876'
+                    'facebook.app_id' => '54321',
+                    'facebook.secret' => '09876',
                 ),
-            )
+            ),
         ));
-        
-        $app->get('/', function() use($app) {
+
+        $app->get('/', function () use ($app) {
             $app['facebook_test'];
             $app['facebook_foo'];
         });
         $request = Request::create('/');
         $app->handle($request);
 
-        $this->assertTrue($app['facebook_test'] instanceof \Facebook);
-        $this->assertEquals($app['facebook_test']->getAppId(), '12345');
-        $this->assertEquals($app['facebook_test']->getApiSecret(), '67890');
-        $this->assertEquals($app['facebook_test']->getAccessToken(), '12345|67890');
+        $this->assertTrue($app['facebook_test'] instanceof Facebook\Facebook);
+        $this->assertEquals($app['facebook_test']->getApp()->getId(), '12345');
+        $this->assertEquals($app['facebook_test']->getApp()->getSecret(), '67890');
+        // $this->assertEquals($app['facebook_test']->getAccessToken(), '12345|67890');
 
-        $this->assertTrue($app['facebook_foo'] instanceof \Facebook);
-        $this->assertEquals($app['facebook_foo']->getAppId(), '54321');
-        $this->assertEquals($app['facebook_foo']->getApiSecret(), '09876');
-        $this->assertEquals($app['facebook_foo']->getAccessToken(), '54321|09876');
+        $this->assertTrue($app['facebook_foo'] instanceof Facebook\Facebook);
+        $this->assertEquals($app['facebook_foo']->getApp()->getId(), '54321');
+        $this->assertEquals($app['facebook_foo']->getApp()->getSecret(), '09876');
+        // $this->assertEquals($app['facebook_foo']->getAccessToken(), '54321|09876');
     }
 }
